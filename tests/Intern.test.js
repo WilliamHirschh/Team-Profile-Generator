9e95ccd76ce3b202120e0c7708ffc236c7ed8632
William Hirsch <Williamhirschh@gmail.com>
2023-02-07 19:51:36 -0600

    bug fixes

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,21 +1,14 @@
-// using Intern constructor 
 const Intern = require('../Employees/intern');
-
-// creating intern object  
 test('creates an Intern object', () => {
     const intern = new Intern('William', 12, 'williamhirsch@gmail.com', 'ENG');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
-
-// gets school from getSchool()
 test('gets employee school', () => {
     const intern = new Intern('William', 12, 'williamhirsch@gmail.com', 'ENG');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
-
-// gets role from getRole()
 test('gets role of employee', () => {
     const intern = new Intern('William', 12, 'williamhirsch@gmail.com', 'ENG');
 
